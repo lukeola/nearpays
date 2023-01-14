c8475c7ec3213fee90aa62883857fb178492dada
Luke Olawale <lukeolawale4@gmail.com>
2023-01-14 03:46:29 +0100

    section 3 completed

--- src/components/sectionthree/SectionThreeElements.js
+++ src/components/sectionthree/SectionThreeElements.js
@@ -5,5 +5,96 @@ export const SectionThreeContainer = styled.div`
     position: relative;
     width: 100%;
     height: 706px;
-    background: #E0EBE3;
+    display: flex;
+    gap: 15%;
+    background: #E0EBE3; 
+
+    @media screen and (max-width: 769px) {
+        flex-direction: column;
+        height: 668px;
+    }
+`
+export const Leftside = styled.div`
+    width: 40%;
+    left: 5%;
+
+    @media screen and (max-width: 769px) {
+        width: 100%;
+    }
+`
+export const LefsideImg = styled.div`
+    position: absolute;
+    width: 90%;
+    height: 425.79px;
+    left: 5%;
+    top: 140px;
+
+    @media screen and (max-width: 769px) {
+        top: 51px;
+        height: 262.79px;
+    }
+`
+export const Leftsidebg = styled.div`
+    position: absolute;
+    width: 38%;
+    height: 594px;
+    left: 8%;
+    top: 56px;
+    background: #C1D3C6;
+    border-radius: 16px;
+
+    @media screen and (max-width: 769px) {
+        width: 90%;
+        left: 5%;
+        height: 364.79px;
+        top: 24px;
+}
+`
+export const Rightside = styled.div`
+    width: 40%;
+    margin-top: 199px;
+
+    @media screen and (max-width: 769px) {
+        width: 90%;
+        margin-top: 350px;
+    }
+  
+`
+export const RightsideHeading = styled.div`
+    width: 80%;
+    height: 81px;
+    font-family: 'Circular Std';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 64px;
+    color: #2E312F;
+    top: 199px;
+
+    @media screen and (max-width: 769px) {
+        height: 51px;
+        width: 90%;
+        font-size: 32px;
+        margin-left: 5%;
+        
+    }
+`
+export const RightsideText = styled.div`
+    width: 60%;
+    font-family: 'Fakt Pro';
+    font-style: normal;
+    font-weight: 300;
+    font-size: 20px;
+    line-height: 210%;
+    color: #2E312F;
+    flex: none;
+    order: 1;
+    flex-grow: 0;
+
+        @media screen and (max-width: 769px) {
+            width: 90%;
+            margin-left: 5%;
+            font-size: 16px;
+            line-height: 150%;
+    
+        }
 `
--- src/components/sectionthree/index.js
+++ src/components/sectionthree/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { SectionThreeContainer } from './SectionThreeElements'
+import { LefsideImg, Leftside, Leftsidebg, Rightside, RightsideHeading, RightsideText, SectionThreeContainer } from './SectionThreeElements'
+import Iphone from '../../images/phone.png'
+
 
 const SectionThree = () => {
   return (
     <SectionThreeContainer>
-      
+      <Leftside>
+        <Leftsidebg>
+          <LefsideImg>
+            <img src={Iphone} alt='' style={{objectFit:'fill', height:'1005', width:'100%'}}/>
+          </LefsideImg>
+        </Leftsidebg>
+      </Leftside>
+
+      <Rightside>
+        <RightsideHeading>Virtual POS</RightsideHeading>
+        <RightsideText>
+        Charge and debit NFC-enabled cards with your smartphone
+Steer your business towards a new level of financial transactions
+   (Your smartphone becomes your POS Terminal)
+        </RightsideText>
+      </Rightside>
     </SectionThreeContainer>
   )
 }
